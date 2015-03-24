34217bf257234f9f14bd59528b575363d2d8fc41
LittleWhiteYA <F74014041@mail.ncku.edu.tw>
2015-03-24 20:29:10 +0800

    lab2

---
+++ lab2/BMI.h
@@ -0,0 +1,15 @@
+#include <string>
+
+using namespace std;
+
+class BMI{
+public:
+    BMI(float height, float weight);
+    float BMI_value();
+    string BMI_category();
+
+private:
+    float hei, wei;
+    float bmi;
+};
+
